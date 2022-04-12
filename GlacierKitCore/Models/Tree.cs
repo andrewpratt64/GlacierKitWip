4ba33abd701252844a9628b98614e9dcd0f02b26
andrewpratt64 <adpratt@yahoo.com>
2022-04-12 17:10:28 -0400

    Refactored code to look less like it was written by a crazy person

---
+++ GlacierKitCore/Models/Tree.cs
@@ -0,0 +1,174 @@
+﻿using DynamicData;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlacierKitCore.Models
+{
+	/// <summary>
+	/// Describes a hierarchy of data
+	/// </summary>
+	/// <typeparam name="TNodeValue">The type of data each node represents</typeparam>
+	public abstract class Tree<TNodeValue> : ReactiveObject
+	{
+		#region Non_public_fields
+
+		internal readonly SourceList<TreeNode<TNodeValue>> _nodes;
+
+		#endregion
+
+
+		#region Public_properties
+
+		/// <summary>
+		/// True if a new root node may be created, false otherwise
+		/// </summary>
+		[Reactive]
+		public abstract bool CanAddRootNode { get; protected set; }
+
+		#endregion
+
+
+		#region Public_methods
+
+		/// <summary>
+		/// Connect to and observe changes of a flat view of all of the tree's nodes
+		/// </summary>
+		/// <returns>An observable that emits the change set of nodes</returns>
+		public IObservable<IChangeSet<TreeNode<TNodeValue>>> ConnectToNodes()
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+
+
+		#region Public_commands
+
+		/// <summary>
+		/// Creates a new root node
+		/// <list type="bullet">
+		///		<item>
+		///				<term>Input</term>
+		///				<description>The value of the node to create</description>
+		///		</item>
+		///		<item>
+		///				<term>Output</term>
+		///				<description>The new root node</description>
+		///		</item>
+		/// </list>
+		/// </summary>
+		/// <remarks></remarks>
+		public abstract ReactiveCommand<TNodeValue, TreeNode<TNodeValue>> CreateRootNode
+		{ get; }
+
+		#endregion
+
+
+		#region Constructor
+
+		/// <summary>
+		/// Creates a new instance of the <see cref="Tree"/> class
+		/// </summary>
+		protected Tree()
+		{
+			throw new NotImplementedException();
+
+			//_nodes = new();
+		}
+
+		#endregion
+	}
+
+
+	/// <summary>
+	/// Describes a <see cref="Tree{TNodeValue}"/> that dosen't support more than one root node
+	/// </summary>
+	/// <typeparam name="TNodeValue"><inheritdoc cref="Tree{TNodeValue}"/></typeparam>
+	public class SingleRootTree<TNodeValue> : Tree<TNodeValue>
+	{
+		#region Public_properties
+
+		/// <summary>
+		/// The root node of the tree, or null if the tree is empty
+		/// </summary>
+		public TreeNode<TNodeValue>? RootNode { get; internal set; }
+
+		#endregion
+
+
+		#region Public_override_properties
+
+		[Reactive]
+		public override bool CanAddRootNode {get; protected set; }
+
+		#endregion
+
+
+		#region Public_override_commands
+
+		public override ReactiveCommand<TNodeValue, TreeNode<TNodeValue>> CreateRootNode
+		{ get; }
+
+		#endregion
+
+
+		#region Constructor
+
+		/// <summary>
+		/// <inheritdoc cref="Tree{TNodeValue}.Tree"/>
+		/// </summary>
+		public SingleRootTree() : base()
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
+
+
+	/// <summary>
+	/// Describes a <see cref="Tree{TNodeValue}"/> that supports any number of root nodes
+	/// </summary>
+	/// <typeparam name="TNodeValue"><inheritdoc cref="Tree{TNodeValue}"/></typeparam>
+	public class MultiRootTree<TNodeValue> : Tree<TNodeValue>
+	{
+		#region Non_public_fields
+
+		internal readonly SourceList<TreeNode<TNodeValue>> _rootNodes;
+
+		#endregion
+
+
+		#region Public_override_properties
+
+		public override bool CanAddRootNode { get; protected set; }
+
+		#endregion
+
+
+		#region Public_override_commands
+
+		public override ReactiveCommand<TNodeValue, TreeNode<TNodeValue>> CreateRootNode
+		{ get; }
+
+		#endregion
+
+
+		#region Constructor
+
+		/// <summary>
+		/// <inheritdoc cref="Tree{TNodeValue}.Tree"/>
+		/// </summary>
+		public MultiRootTree() : base()
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+	}
+}
---
+++ GlacierKitCore/Models/TreeNode.cs
@@ -0,0 +1,227 @@
+﻿using Avalonia.Data;
+using DynamicData;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlacierKitCore.Models
+{
+	/// <summary>
+	/// A single node within a <see cref="Tree{TNodeValue}"/>
+	/// </summary>
+	/// <typeparam name="TNodeValue">The type of data this node represents</typeparam>
+	/// <remarks>This is used instead of DynamicData's <see cref="DynamicData.Node{TObject, TKey}"/> to restrict how nodes are constructed and to avoid re-generating trees</remarks>
+	public abstract class TreeNode<TNodeValue> : ReactiveObject
+	{
+		#region Private_fields
+
+		private SourceList<TreeNode<TNodeValue>> _childNodes;
+
+		#endregion
+
+
+		#region Public_methods
+
+		/// <summary>
+		/// Connect to and observe changes of this node's direct children
+		/// </summary>
+		/// <returns>An observable that emits the change set of child nodes</returns>
+		public IObservable<IChangeSet<TreeNode<TNodeValue>>> ConnectToChildNodes()
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+
+
+		#region Public_properties
+
+		/// <summary>
+		/// The tree this node is a part of
+		/// </summary>
+		public Tree<TNodeValue> ContainingTree { get; }
+
+		/// <summary>
+		/// The value this node represents
+		/// </summary>
+		[Reactive]
+		public TNodeValue Value { get; set; }
+
+		/// <summary>
+		/// This node's parent, or null if this is a root node
+		/// </summary>
+		[Reactive]
+		public TreeNode<TNodeValue>? Parent { get; set; }
+
+		/// <summary>
+		/// The node that this node would like to reparent to, if any
+		/// </summary>
+		/// <remarks>
+		/// Whether or not this node may be reparented to it's desired node depends on <seealso cref="CanReparent"/>.
+		/// <br/>
+		/// <list type="table">
+		///		<listheader>
+		///			<term>If this property is...</term>
+		///			<description>Then...</description>
+		///		</listheader>
+		///		<item>
+		///				<term>Empty</term>
+		///				<description>This node does not wish to be reparented</description>
+		///		</item>
+		///		<item>
+		///				<term>Not empty with a null value</term>
+		///				<description>This node wishes to become a root node</description>
+		///		</item>
+		///		<item>
+		///				<term>Not empty with a non-null value</term>
+		///				<description>This node wishes to become a child node of another node</description>
+		///		</item>
+		/// </list>
+		/// </remarks>
+		[Reactive]
+		public Optional<TreeNode<TNodeValue>?> DesiredParent { get; set; }
+
+		#endregion
+
+
+		#region Public_commands
+
+		/// <summary>
+		/// Creates a new child node
+		/// <list type="bullet">
+		///		<item>
+		///				<term>Input</term>
+		///				<description>The value of the node to create</description>
+		///		</item>
+		///		<item>
+		///				<term>Output</term>
+		///				<description>The new child node</description>
+		///		</item>
+		/// </list>
+		/// </summary>
+		/// <remarks></remarks>
+		public ReactiveCommand<TNodeValue, TreeNode<TNodeValue>> AddChild
+		{ get; }
+
+		/// <summary>
+		/// 
+		/// <list type="bullet">
+		///		<item>
+		///				<term>Input</term>
+		///				<description>True to delete the children of this node recursively as well, false to move all children up to this node's depth instead</description>
+		///		</item>
+		///		<item>
+		///				<term>Output</term>
+		///				<description>(none)</description>
+		///		</item>
+		/// </list>
+		/// </summary>
+		/// <remarks></remarks>
+		public ReactiveCommand<bool, Unit> Delete
+		{ get; }
+
+		/// <summary>
+		/// 
+		/// <list type="bullet">
+		///		<item>
+		///				<term>Input</term>
+		///				<description>True to delete the children of this node recursively as well, false to move all children up to this node's depth instead</description>
+		///		</item>
+		///		<item>
+		///				<term>Output</term>
+		///				<description>(none)</description>
+		///		</item>
+		/// </list>
+		/// </summary>
+		/// <remarks></remarks>
+		public ReactiveCommand<Unit, Unit> Reparent
+		{ get; }
+
+		#endregion
+
+
+		#region Public_OAPHs
+
+		/// <summary>
+		/// True if this is a root node, false otherwise
+		/// </summary>
+		[ObservableAsProperty]
+		public bool IsRootNode{ get; }
+
+		/// <summary>
+		/// True if this node may be deleted, false otherwise
+		/// </summary>
+		[ObservableAsProperty]
+		public bool CanDelete { get; }
+
+		/// <summary>
+		/// True if this node may be deleted along with all of it's direct and indirect children, false otherwise
+		/// </summary>
+		[ObservableAsProperty]
+		public bool CanRecursivelyDelete { get; }
+
+		/// <summary>
+		/// True if this node may reparent to the node stated by <see cref="DesiredParent"/>, false otherwise
+		/// </summary>
+		[ObservableAsProperty]
+		public bool CanReparent { get; }
+
+		#endregion
+
+
+		#region Constructor
+
+		/// <summary>
+		/// Creates a new instance of the <see cref="TreeNode{TNodeValue}"/> class
+		/// </summary>
+		/// <param name="containingTree"><inheritdoc cref="ContainingTree"/></param>
+		/// <param name="nodeValue"><inheritdoc cref="Value"/></param>
+		/// <param name="parent"><inheritdoc cref="Parent"/></param>
+		protected TreeNode(Tree<TNodeValue> containingTree, TNodeValue nodeValue, TreeNode<TNodeValue>? parent)
+		{
+			throw new NotImplementedException();
+			/*
+			_childNodes = new();
+
+			ContainingTree = containingTree;
+			Value = nodeValue;
+			Parent = parent;
+
+			DesiredParent = Optional<TreeNode<TNodeValue>?>.Empty;
+
+			AddChild = ReactiveCommand.Create<TNodeValue, TreeNode<TNodeValue>>(
+				execute: nodeValue =>
+				{
+					throw new NotImplementedException();
+				}
+				//canExecute: TODO
+			);
+
+			Delete = ReactiveCommand.Create<bool, Unit>(
+				execute: nodeValue =>
+				{
+					throw new NotImplementedException();
+				}
+				//canExecute: TODO
+			);
+
+			Reparent = ReactiveCommand.Create<Unit, Unit>(
+				execute: nodeValue =>
+				{
+					throw new NotImplementedException();
+				}
+				//canExecute: TODO
+			);
+			*/
+		}
+
+		#endregion
+	}
+}
